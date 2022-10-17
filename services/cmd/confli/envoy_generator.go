fc2167e57cd4a81e519b8828b6dd5d14d77da34a
abhisek <abhisek.datta@gmail.com>
2022-10-17 10:48:58 +0530

    Add internal service cluster

--- services/cmd/confli/envoy_generator.go
+++ services/cmd/confli/envoy_generator.go
@@ -17,9 +17,20 @@ import (
 	envoy_endpoint_v3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	envoy_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	envoy_extension_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+	envoy_extension_http_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/upstreams/http/v3"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
+)
+
+const (
+	pdpSvcName  = "ext-authz-pdp"
+	pdpHostName = "pdp"
+	PdpPort     = "9001"
+
+	tapSvcName  = "ext-proc-tap"
+	tapHostName = "tap"
+	tapPort     = "9001"
 )
 
 /**
@@ -190,7 +201,81 @@ func envoyGenerateStaticClusters(gateway *gen.GatewayConfiguration) ([]*envoy_cl
 		clusters = append(clusters, cluster)
 	}
 
+	pdpCluster, err := envoyGetInternalServiceCluster(pdpSvcName, pdpHostName, PdpPort)
+	if err != nil {
+		return clusters, fmt.Errorf("failed to generate pdp cluster: %w", err)
+	}
+
+	clusters = append(clusters, pdpCluster)
+
+	tapCluster, err := envoyGetInternalServiceCluster(tapSvcName, tapHostName, tapPort)
+	if err != nil {
+		return clusters, fmt.Errorf("failed to generate tap cluster: %w", err)
+	}
+
+	clusters = append(clusters, tapCluster)
+
 	return clusters, nil
+}
+
+func envoyGetInternalServiceCluster(svcName, hostName, port string) (*envoy_cluster_v3.Cluster, error) {
+	cluster := &envoy_cluster_v3.Cluster{
+		Name: svcName,
+		LoadAssignment: &envoy_endpoint_v3.ClusterLoadAssignment{
+			ClusterName: svcName,
+			Endpoints:   make([]*envoy_endpoint_v3.LocalityLbEndpoints, 0),
+		},
+		TypedExtensionProtocolOptions: map[string]*anypb.Any{},
+	}
+
+	explicitHttpConfig := &envoy_extension_http_v3.HttpProtocolOptions{
+		UpstreamProtocolOptions: &envoy_extension_http_v3.HttpProtocolOptions_ExplicitHttpConfig_{
+			ExplicitHttpConfig: &envoy_extension_http_v3.HttpProtocolOptions_ExplicitHttpConfig{
+				ProtocolConfig: &envoy_extension_http_v3.HttpProtocolOptions_ExplicitHttpConfig_Http2ProtocolOptions{
+					Http2ProtocolOptions: &envoy_core_v3.Http2ProtocolOptions{},
+				},
+			},
+		},
+	}
+
+	explicitHttpConfigData, err := proto.Marshal(explicitHttpConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize proto message: %w", err)
+	}
+
+	cluster.TypedExtensionProtocolOptions["envoy.extensions.upstreams.http.v3.HttpProtocolOptions"] = &anypb.Any{
+		TypeUrl: "type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions",
+		Value:   explicitHttpConfigData,
+	}
+
+	endpoint := &envoy_endpoint_v3.LocalityLbEndpoints{
+		LbEndpoints: make([]*envoy_endpoint_v3.LbEndpoint, 0),
+	}
+
+	portValue, err := strconv.Atoi(port)
+	if err != nil {
+		return cluster, fmt.Errorf("failed to convert port from string to uint: %w", err)
+	}
+
+	lb_endpoint := &envoy_endpoint_v3.LbEndpoint{
+		HostIdentifier: &envoy_endpoint_v3.LbEndpoint_Endpoint{
+			Endpoint: &envoy_endpoint_v3.Endpoint{
+				Address: &envoy_core_v3.Address{
+					Address: &envoy_core_v3.Address_SocketAddress{
+						SocketAddress: &envoy_core_v3.SocketAddress{
+							Address:       hostName,
+							PortSpecifier: &envoy_core_v3.SocketAddress_PortValue{PortValue: uint32(portValue)},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	endpoint.LbEndpoints = append(endpoint.LbEndpoints, lb_endpoint)
+	cluster.LoadAssignment.Endpoints = append(cluster.LoadAssignment.Endpoints, endpoint)
+
+	return cluster, nil
 }
 
 func envoyNodeId(gid string) string {
